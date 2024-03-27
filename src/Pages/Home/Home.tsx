011e0ecabdf45555ce1e58e6157226c00f650414
yannickwnz <yannickwnz@gmail.com>
2024-03-27 06:16:57 +0100

    working on home

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.scss'
+
+export const Home = () => {
+    return (
+        <section className="home">
+            <div className="home-container">
+                <aside className="sidebar"></aside>
+                <div className="feedback-feed"></div>
+            </div>
+        </section>
+    )
+}

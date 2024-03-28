0ee5e6fb06ce0d6ebe067f1bcac9fad016551210
yannickwnz <yannickwnz@gmail.com>
2024-03-28 11:59:44 +0100

    working on home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import './Home.scss'
 
 export const Home = () => {
 
-    // const features
+    const [isSelected, setIsSelected] = useState<boolean>(false)
+    const [isFeatureSelected, setIsFeatureSelected] = useState<boolean>(false)
+    const [isBugSelected, setIsBugSelected] = useState<boolean>(false)
+    // const [isSelected, setIsSelected] = useState<boolean>(false)
+
+    const features = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature']
+
+    const [selectedFeature, setSelectedFeature] = useState(features[0]);
+
+    const suggestionsCount: number = 19
+
+    const handleClick = (feature: string) => {
+        setSelectedFeature(feature);
+    };
+
+
+    // update state if localStorage is set
+    useEffect(() => {
+
+    let selectedFeatureFromLocalStorage = localStorage.getItem('selectedFeature')
+    if(selectedFeatureFromLocalStorage) setSelectedFeature(selectedFeatureFromLocalStorage)
+
+    }, [selectedFeature])
+
+    const selectFeature = (feature: string): void => {
+
+        setSelectedFeature(feature)
+
+        // set feature to local storage
+        localStorage.setItem('selectedFeature', feature)
+
+    }
+
+
+
 
     return (
         <section className="home">
@@ -15,14 +49,27 @@ export const Home = () => {
                         <p>Feedback Board</p>
                     </div>
                     <div className="features-section">
-                        <ul>
+                        {/* <ul>
                             <li>All</li>
                             <li>UI</li>
                             <li>UX</li>
                             <li>Enhancement</li>
                             <li>Bug</li>
                             <li>Feature</li>
+                        </ul> */}
+
+                        <ul>
+                            {features.length > 0 && features.map((feature, index) => (
+                                <li
+                                    key={index}
+                                    className={`${selectedFeature === feature ? 'selected-feature' : ''}`}
+                                    onClick={() => {
+                                        selectFeature(feature)
+                                    }}
+                                >{feature}</li>
+                            ))}
                         </ul>
+
                     </div>
                     <div className="roadmap">
                         <div className="section-name">
@@ -48,7 +95,43 @@ export const Home = () => {
                     </div>
                 </aside>
                 <div className="feedback-feed">
-                    <h1>THIS IS THE FEED SUCKER</h1>
+                    <nav>
+                        <div className="first-nav-section">
+                            <img src="../starter-code/assets/suggestions/icon-suggestions.svg" alt="" />
+                            <div className="suggestions-count">
+                                <h1>{suggestionsCount} {`${suggestionsCount == 1 ? 'Suggestion' : 'Suggestions'}`}</h1>
+                            </div>
+                            <div className="filter-section">
+                                <label htmlFor="filters">Sort by:</label>
+                                <select name="filters" id="">
+                                    <option value="Most upvotes">Most upvotes</option>
+                                    <option value="Least upvotes">Least upvotes</option>
+                                    <option value="Most comments">Most comments</option>
+                                    <option value="Least comments">Least comments</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="add-feedback-btn">
+                            <Link to='/new-feedback'><img src="../starter-code/assets/shared/icon-new-feedback.svg" alt="" /> <span>Add Feedback</span></Link>
+                        </div>
+
+                    </nav>
+                    <div className="feed-section">
+                        <div className="feed-first-section">
+                            <div className="votes-count">
+                                <img src="../starter-code/assets/shared/icon-arrow-up.svg" alt="" />
+                                <span>6</span>
+                            </div>
+                            <div className="suggestion-details">
+                                <h1>Add tags for solutions</h1>
+                                <p>Easier to search for solutions based on specific stack</p>
+                                <ul>
+                                    <li>Feature</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="comments-number"></div>
+                    </div>
                 </div>
             </div>
         </section>
